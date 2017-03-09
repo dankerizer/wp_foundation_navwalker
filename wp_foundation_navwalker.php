4c3455607ee774eb5f3fcf7e00ca6ae411ecff98
Hadie Danker <hadie87@gmail.com>
2017-03-09 22:52:24 +0700

    pertama
    pertama

---
+++ wp_foundation_navwalker.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * harganya Project
+ * @package wp_foundation_navwalker
+ * User: dankerizer
+ * Date: 09/03/2017 / 22.30
+ */
+class wp_foundation_navwalker extends Walker_Nav_Menu {
+
+    /**
+     * Starts the list before the elements are added.
+     *
+     * Adds classes to the unordered list sub-menus.
+     *
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param int    $depth  Depth of menu item. Used for padding.
+     * @param array  $args   An array of arguments. @see wp_nav_menu()
+     */
+    function start_lvl( &$output, $depth = 0, $args = array() ) {
+        // Depth-dependent classes.
+        $indent = ( $depth > 0  ? str_repeat( "\t", $depth ) : '' ); // code indent
+        $display_depth = ( $depth + 1); // because it counts the first submenu as 0
+        $classes = array(
+            ' menu   li',
+            ( $display_depth % 2  ? 'menu-odd' : 'menu-even' ),
+            ( $display_depth >=2 ? 'sub-sub-menu' : '' ),
+            'menu-depth-' . $display_depth
+        );
+        $class_names = implode( ' ', $classes );
+
+        // Build HTML for output.
+        $output .= "\n" . $indent . '<ul class=" ' . $class_names . '">' . "\n";
+    }
+    function end_lvl( &$output, $depth = 0, $args = array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "$indent\n ";
+    }
+    /**
+     * Start the element output.
+     *
+     * Adds main/sub-classes to the list items and links.
+     *
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param object $item   Menu item data object.
+     * @param int    $depth  Depth of menu item. Used for padding.
+     * @param array  $args   An array of arguments. @see wp_nav_menu()
+     * @param int    $id     Current item ID.
+     */
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+        global $wp_query;
+        $class_names = $value = '';
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        $classes[] = 'menu-item-' . $item->ID;
+
+        $indent = ( $depth > 0 ? str_repeat( "\t", $depth ) : '' ); // code indent
+
+        // Depth-dependent classes.
+        $depth_classes = array(
+            ( $depth == 0 ? 'main-menu-item' : 'sub-menu-item' ),
+            ( $depth >=2 ? 'sub-sub-menu-item' : '' ),
+            ( $depth % 2 ? 'menu-item-odd' : 'menu-item-even' ),
+            'menu-item-depth-' . $depth
+        );
+        $depth_class_names = esc_attr( implode( ' ', $depth_classes ) );
+
+        if ( in_array( 'current-menu-item', $classes ) )
+            $class_names .= ' active ';
+        // Passed classes.
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        $class_names .= esc_attr( implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) ) );
+
+        // Build HTML.
+        $output .= $indent . '<li id="nav-menu-item-'. $item->ID . '" class="' . $depth_class_names . ' ' . $class_names . '">';
+
+        // Link attributes.
+        $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+        $attributes .= ' class="menu-link ' . ( $depth > 0 ? 'sub-menu-link' : 'main-menu-link' ) . '"';
+
+        // Build HTML output and pass through the proper filter.
+        $item_output = sprintf( '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
+            $args->before,
+            $attributes,
+            $args->link_before,
+            apply_filters( 'the_title', $item->title, $item->ID ),
+            $args->link_after,
+            $args->after
+        );
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
+    /**
+     * Menu Fallback
+     * =============
+     * If this function is assigned to the wp_nav_menu's fallback_cb variable
+     * and a manu has not been assigned to the theme location in the WordPress
+     * menu manager the function with display nothing to a non-logged in user,
+     * and will add a link to the WordPress menu manager if logged in as an admin.
+     *
+     * @param array $args passed from the wp_nav_menu function.
+     *
+     */
+    public static function fallback( $args ) {
+        if ( current_user_can( 'manage_options' ) ) {
+
+            extract( $args );
+
+            $fb_output = null;
+
+            if ( $container ) {
+                $fb_output = '<' . $container;
+
+                if ( $container_id )
+                    $fb_output .= ' id="' . $container_id . '"';
+
+                if ( $container_class )
+                    $fb_output .= ' class="' . $container_class . '"';
+
+                $fb_output .= '>';
+            }
+
+            $fb_output .= '<ul  ';
+
+            if ( $menu_id )
+                $fb_output .= ' id="' . $menu_id . '"';
+
+            if ( $menu_class )
+                $fb_output .= ' class=" menu ' . $menu_class . '"';
+
+            $fb_output .= '>';
+            $fb_output .= '<li><a itemprop="url" href="' . admin_url( 'nav-menus.php' ) . '">Add a menu</a></li>';
+            $fb_output .= '</ul>';
+
+            if ( $container )
+                $fb_output .= '</' . $container . '>';
+
+            echo $fb_output;
+        }
+    }
+}
